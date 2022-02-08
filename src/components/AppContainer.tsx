a94ebc9f7ba5f3d19a940a83b594c22554617002
Erik Pizana <erikpzcanedo@outlook.com>
2022-02-08 17:30:51 +0100

    Drawer

--- src/components/AppContainer.tsx
+++ src/components/AppContainer.tsx
@@ -1,12 +1,33 @@
-import { Box, styled } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
+import { Box, Drawer, IconButton, styled } from "@mui/material";
+import { DisplaySettings } from "@mui/icons-material";
 
 const RootStyle = styled(Box)(({ theme }) => ({
   width: "100%",
   height: "100%",
   minHeight: "100%",
+  position: "relative",
+}));
+const ButtonStyle = styled(IconButton)(({ theme }) => ({
+  position: "fixed",
+  right: 0,
+  top: "50%",
+  boxShadow: theme.shadows[12],
 }));
 
 export const AppContainer = ({ children }: any) => {
-  return <RootStyle>{children}</RootStyle>;
+  const [open, setopen] = useState(false);
+  const handleOpen = () => setopen(true);
+  const handleClose = () => setopen(false);
+  return (
+    <RootStyle>
+      <ButtonStyle onClick={handleOpen}>
+        <DisplaySettings />
+      </ButtonStyle>
+      <Drawer anchor="right" open={open} onClose={handleClose}>
+        Drawer
+      </Drawer>
+      {children}
+    </RootStyle>
+  );
 };
